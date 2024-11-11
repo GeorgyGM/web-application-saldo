ebb75e63f275c9ba7d61e4677ccc88ccf8a8836c
GeorgyGM <georgygevorkov@gmail.com>
2024-11-11 12:37:23 +0300

    Add files via upload
    Zagotovka web-prilozheniya

---
+++ ApplicationContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+public class ApplicationContext : DbContext
+{
+
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+    {
+        Database.EnsureDeleted();
+        Database.EnsureCreated();   // создаем базу данных при первом обращении
+        //Database.EnsureDeleted();
+         //{ Database.EnsureDeleted(); }
+    }
+    ~ApplicationContext() { Database.EnsureDeleted(); }
+    //public ApplicationContext
+    public DbSet<saldoRow> saldoRows { get; set; } = null!;
+    public DbSet<User> Users { get; set; } = null!;
+    
+    
+    
+   /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Nam = "Tom", Age = 37 },
+                new User { Id = 2, Nam = "Bob", Age = 41 },
+                new User { Id = 3, Nam = "Sam", Age = 24 }
+        );
+        modelBuilder.Entity<saldoRow>().ToTable("Users", "log");
+    }*/
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<saldoRow>().HasData(
+            new saldoRow { Id = 1, perio = "May 2024" , saldoIncome = 0, nachisleno = 50, 
+                           perer = -1, itogoNach = 49, oplach = 5, saldoOutcome = 44 },
+            new saldoRow
+            {
+                Id = 2,
+                perio = "May 2024",
+                saldoIncome = 0,
+                nachisleno = 50,
+                perer = -1,
+                itogoNach = 49,
+                oplach = 5,
+                saldoOutcome = 44
+               }); 
+               modelBuilder.Entity<saldoRow>().ToTable("saldoRows", "log");
+    }
+}
---
+++ saldoRow.cs
@@ -0,0 +1,18 @@
+﻿public class User
+{
+    public int Id { get; set; }
+    public string Nam { get; set; } = ""; // имя пользователя
+    public int Age { get; set; } // возраст пользователя
+}
+public class saldoRow
+{
+    public int Id { get; set; }
+    public string perio { get; set; } = "";
+    public decimal saldoIncome { get; set; }
+    public decimal nachisleno { get; set; }
+    public decimal perer { get; set; }
+    public decimal itogoNach { get; set; }
+    public decimal oplach { get; set; }
+    public decimal saldoOutcome { get; set; }
+
+}
